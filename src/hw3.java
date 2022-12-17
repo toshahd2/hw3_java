d3ab9226786f3eea8bb4c8aaadefad6c9c587dee
Anton <tosha.hd2@gmail.com>
2022-12-17 14:05:55 +0500

    task 2-4

--- src/hw3.java
+++ src/hw3.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class hw3 {
     public static void main(String[] args) {
         //1. Создать новый список, добавить несколько строк и вывести коллекцию на экран.
@@ -10,5 +11,24 @@ public class hw3 {
         stringList.add("Добавили вторую строку");
         stringList.add("Добавили третью строку");
         System.out.println(Arrays.toString(stringList.toArray()));
+
+        //2. Итерация всех элементов списка цветов и добавления к каждому символа '!'.
+        System.out.println("\nЗадание 2");
+        String[] cmykArray = new String[]{"Cyan", "Magenta", "Yellow", "Key"};
+        ArrayList<String> cmykList = new ArrayList<>();
+        for (int i = 0; i < cmykArray.length; i++) {
+            cmykList.add(cmykArray[i] + "!");
+        }
+        System.out.println(Arrays.toString(cmykList.toArray()));
+
+        //3. Вставить элемент в список в первой позиции.
+        System.out.println("\nЗадание 3");
+        stringList.add(0, "Самая первая строка");
+        System.out.println(Arrays.toString(stringList.toArray()));
+
+        //4. Извлечь элемент (по указанному индексу) из заданного списка.
+        System.out.println("\nЗадание 4");
+        String extractedElement = stringList.remove(2);
+        System.out.println(extractedElement);
     }
 }
